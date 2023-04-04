f4597e2fd76d50c4415fc15440027af9496dd83e
Henry <henhen1227@gmail.com>
2023-04-04 12:29:25 -0400

    Added a quick home page

---
+++ index.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const swingElements = document.querySelectorAll(".swing");
+//     if (swingElements.length > 1) {
+//         swingElements[1].style.animationDelay = "0.75s";
+//     }
+// });
